250f8ce4b5c737a5b809f1baa7b4596f6187b284
denini08 <denini.silva@upe.br>
2017-08-31 21:06:57 -0300

    startt'

--- jogo_velha.cpp
+++ jogo_velha.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 class Jogo_velha{
-	public:
-		Jogo_velha(){
-			for(int i = 0; i <3 ; i++){
-				for(int j = 0; j < 3; j++){
-					matrix[i][j] = 0;
-				}
+ public:
+	Jogo_velha(){
+		for(int i = 0; i <3 ; i++){
+			for(int j = 0; j < 3; j++){
+				matrix[i][j] = 0;
 			}
 		}
-		int getposicao(int x, int y){
-			return matrix[x][y];
+	}
+	int getposicao(int x, int y){
+		return matrix[x][y];
+	}
+	int setposicao(int x, int y,  int jogador){
+		if(matrix[x][y] == 0 ){ //caso a posicao nao foi jogada
+			matrix[x][y] = jogador;
+			return 0; 
 		}
-		int setposicao(int x, int y,  int jogador){
-			if(matrix[x][y] != 0 ){ //caso a posicao já foi jogada
-				matrix[x][y] = jogador;
-				return 0; 
-			}
-			else{
-				return -1; //caso erro
-			}
+		else{
+			return -1; //caso erro
 		}
-	private:
-		int matrix[3][3];
+	}
+ private:
+	int matrix[3][3];
 };
 
 
