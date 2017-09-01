25507567570e78ba4081fb43a48176d0873d4700
denini08 <denini.silva@upe.br>
2017-08-31 22:00:13 -0300

    tomando forma

--- jogo_velha.cpp
+++ jogo_velha.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 class Jogo_velha{
  public:
 	Jogo_velha(){
@@ -23,13 +24,81 @@ class Jogo_velha{
 			return -1; //caso erro
 		}
 	}
+	void imprimir(){
+		//pregica de fazer :S
+	}
  private:
 	int matrix[3][3];
 };
 
+class Player{
+ public:
+	Player(string nome_, int numero_){		//numero ? 1 ou 2
+		nome = nome_;
+		numero = numero_;
+	}
+	
+	int getnumero(){
+		return numero;
+    }
+	string getnome(){
+		return nome;
+	}
+ 
+ private:
+	int numero;
+	string nome;
+};
 
 
 int main(const int argc, const char **argv){
+	string a, b;
+	int x = 3,y = 3, p = 0;
 	
+	
+	cout << "Jogador 1 digite seu nome:" << endl;
+	cin >> a ;
+	cout << "Jogador 2 digite seu nome:" << endl;
+	cin >> b;
+	Jogo_velha *obj = new Jogo_velha();
+	Player *p1 = new Player(a , 1);
+	Player *p2 = new Player(b , 2);
+	
+	
+	while(true){
+		loop:
+		if(p == 0){
+			cout << p1->getnome() << " digite a posicao que deseja jogar:" << endl;
+			scanf("%d %d",&x,&y );
+			if(x > 2 || x < 0 || y>2 || y < 0){
+				cout << "posicao inexistente"<< endl;
+				goto loop;
+			}
+			if(obj->setposicao(x,y,p1->getnumero()) == -1){
+				cout << p1->getnome() << " digitou uma posicao ja jogada anteriormente" << endl;
+				goto loop;
+			} else printf("FOIIIIII\n");
+			p++;
+			
+		}
+		else{
+			cout << p2->getnome() << " digite a posicao que deseja jogar:" << endl;
+			scanf("%d %d",&x,&y );
+			if(x > 2 || x < 0 || y>2 || y < 0){
+				cout << "posicao inexistente"<< endl;
+				goto loop;
+			}
+			if(obj->setposicao(x,y,p2->getnumero()) == -1){
+				cout << p2->getnome() << " digitou uma posicao ja jogada anteriormente" << endl;
+				goto loop;
+			}
+			p--;
+			
+		}
+	}
 	return 0;
 }
+
+
+
+
